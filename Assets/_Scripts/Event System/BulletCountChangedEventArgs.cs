c0cdc226894d91a38dfb9c6052a7df4c519f08a6
Yoldaş Ulaş <yutalas@gmail.com>
2024-10-24 11:33:04 +0300

    Bullet Count Changed and Level End Event args classes created.AI Agent Config Scriptable Object created

---
+++ Assets/_Scripts/Event System/BulletCountChangedEventArgs.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace _Scripts.Event_System
+{
+	public class BulletCountChangedEventArgs : EventArgs
+	{
+		public int bulletCount;
+	}
+}
---
+++ Assets/_Scripts/Event System/LevelEndEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace _Scripts.Event_System
+{
+	public class LevelEndEventArgs : EventArgs
+	{
+		public bool isLevelCompleted;
+		public int remainingTime;
+	}
+}
