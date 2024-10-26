ca530e0c591851d918b7a01d48f0122285f44792
Yoldaş Ulaş <yutalas@gmail.com>
2024-10-26 08:08:20 +0300

    Coin Spawner, Enemy Spawner classes  and their instance prefabs are created. Game Scene changed

---
+++ Assets/_Scripts/Spawners/CoinSpawner.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using _Scripts.Event_System;
+using _Scripts.Object_Pool;
+using _Scripts.Player;
+using Object_Pool;
+using UnityEngine;
+
+namespace _Scripts.Spawners
+{
+	//Also it can give different things based on the enemy type in the future like health, ammo etc.That would make it more like a DropSpawner class
+	public class CoinSpawner : MonoBehaviour
+	{
+		private const float max_speed = 10;
+		private const float coin_start_y = 0.75f;
+		[SerializeField]
+		private GameObject coinPrefab;
+		[SerializeField]
+		private AnimationCurve coinSpeedCurve;
+		private readonly WaitForSeconds animationWaitForSeconds = new WaitForSeconds(3.5f);
+		private ObjectPool coinPool;
+		private Transform playerTransform;
+
+		private void Awake()
+		{
+			coinPool = ObjectPoolManager.CreateInstance(coinPrefab.GetComponent<PoolAbleObject>(), 50);
+			playerTransform = FindObjectOfType<PlayerController>().transform;
+		}
+
+		private void OnEnable()
+		{
+			EventManager.Subscribe<EnemyDamagedEventArgs>(SpawnCoin);
+		}
+
+		private void OnDisable()
+		{
+			EventManager.UnSubscribe<EnemyDamagedEventArgs>(SpawnCoin);
+		}
+
+		private void SpawnCoin(EnemyDamagedEventArgs data)
+		{
+			if (!data.isDead)
+			{
+				return;
+			}
+
+			PoolAbleObject coin = coinPool.GetObject(new Vector3(data.Position.x, coin_start_y, data.Position.z), Quaternion.identity);
+			coin.transform.rotation = Quaternion.Euler(90, 0, 0);
+			StartCoroutine(CoinAnimation(playerTransform, coin));
+		}
+
+		private IEnumerator CoinAnimation(Transform playerTransform, PoolAbleObject coin)
+		{
+			Vector3 coinPosition = coin.transform.position;
+			yield return animationWaitForSeconds;
+
+			float time = Time.deltaTime;
+
+			while (Vector3.Distance(coinPosition, playerTransform.position) > 0.2f)
+			{
+				float speed = coinSpeedCurve.Evaluate(time) * max_speed;
+				time += Time.deltaTime;
+				coinPosition = Vector3.MoveTowards(coin.transform.position, playerTransform.position, speed);
+				coin.transform.position = coinPosition;
+				yield return null;
+			}
+
+			coin.gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Assets/_Scripts/Spawners/EnemySpawner.cs
@@ -0,0 +1,73 @@
+﻿using _Scripts.AI;
+using _Scripts.Object_Pool;
+using _Scripts.Player;
+using Object_Pool;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace _Scripts.Spawners
+{
+	public class EnemySpawner : MonoBehaviour
+	{
+		[SerializeField]
+		private GameObject enemyPrefab;
+		[SerializeField]
+		private float spawnRadius = 20f;
+		[SerializeField]
+		private float spawnCooldown = 1f;
+		[SerializeField]
+		private Transform playerTransform;
+		private float counter;
+		private EnemyDetector enemyDetector;
+		private Camera mainCamera;
+		private ObjectPool objectPool;
+
+		private void Start()
+		{
+			mainCamera = Camera.main;
+			counter = spawnCooldown;
+			enemyDetector = playerTransform.GetComponentInChildren<EnemyDetector>();
+			objectPool = ObjectPoolManager.CreateInstance(enemyPrefab.GetComponent<PoolAbleObject>(), 50);
+			SpawnEnemy();
+		}
+
+		private void FixedUpdate()
+		{
+			counter -= Time.fixedDeltaTime;
+			if (!(counter <= 0)) return;
+			counter = spawnCooldown;
+			SpawnEnemy();
+		}
+
+		private void SpawnEnemy()
+		{
+			if (!TryGetSpawnPoint(out Vector3 spawnPoint)) return;
+			PoolAbleObject poolAbleObject = objectPool.GetObject(spawnPoint, Quaternion.identity);
+			poolAbleObject.GetComponent<BaseAIAgent>().playerTransform = playerTransform;
+			poolAbleObject.GetComponent<AgentHealth>().EnemyDetector = enemyDetector;
+		}
+
+		private bool TryGetSpawnPoint(out Vector3 spawnPoint)
+		{
+			Vector3 playerPosition = playerTransform.position;
+
+			for (int i = 0; i < 30; i++)
+			{
+				Vector3 randomPoint = playerPosition + Random.insideUnitSphere * spawnRadius;
+				randomPoint.y = playerPosition.y;
+				if (!IsValidSpawnPoint(randomPoint)) continue;
+				spawnPoint = randomPoint;
+				return true;
+			}
+
+			spawnPoint = Vector3.zero;
+			return false;
+		}
+
+		private bool IsValidSpawnPoint(Vector3 point)
+		{
+			Vector3 viewportPoint = mainCamera.WorldToViewportPoint(point);
+			return viewportPoint.x < 0 || viewportPoint.x > 1 || viewportPoint.y < 0 || viewportPoint.y > 1 || viewportPoint.z <= 0;
+		}
+	}
+}
